6fc3ac226311139fc7e53fe3a2db1a3ea5c2032c
Alessandro Gardini <alessandro.gardini7@studio.unibo.it>
2025-11-21 13:54:26 +0100

    add init code to drone-hangar and drone-remote-unit

--- assignment-02/drone-hangar/src/main.cpp
+++ assignment-02/drone-hangar/src/main.cpp
@@ -1,18 +1,50 @@
 #include <Arduino.h>
+#include "config.h"
+#include "kernel/Scheduler.h"
+#include "kernel/Logger.h"
+#include "kernel/MsgService.h"
+#include "model/HWPlatform.h"
+#include "tasks/TestHWTask.h"
+#include "tasks/SweepingTask.h"
+#include "tasks/BlinkingTask.h"
 
-// put function declarations here:
-int myFunction(int, int);
+// #define __TESTING_HW__
+
+Scheduler sched;
+
+HWPlatform* pHWPlatform;
+Context* pContext;
 
 void setup() {
-  // put your setup code here, to run once:
-  int result = myFunction(2, 3);
+  MsgService.init();
+  sched.init(50);
+
+  Logger.log(":::::: Sweeping System ::::::");
+  
+  pHWPlatform = new HWPlatform();
+  pHWPlatform->init();
+
+#ifndef __TESTING_HW__
+  pContext = new Context();
+
+  Task* pSweepingTask = new SweepingTask(pHWPlatform->getButton(), pHWPlatform->getMotor(), pContext);
+  pSweepingTask->init(50);
+
+  Task* pBlinkingTask = new BlinkingTask(pHWPlatform->getLed(), pContext);
+  pBlinkingTask->init(100);
+
+  sched.addTask(pSweepingTask);
+  sched.addTask(pBlinkingTask);
+#endif
+
+#ifdef __TESTING_HW__
+  /* Testing */
+  Task* pTestHWTask = new TestHWTask(pHWPlatform);
+  pTestHWTask->init(50);
+  sched.addTask(pTestHWTask);
+#endif
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
-}
-
-// put function definitions here:
-int myFunction(int x, int y) {
-  return x + y;
+    sched.schedule();
 }
