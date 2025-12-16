c1f4ec9e8057993f25356f0b9bb1f1f70182c113
Alessandro Gardini <alessandro.gardini7@studio.unibo.it>
2025-12-16 21:31:10 +0100

    add msgs consts and basic SerialHadler structure

--- assignment-03/CUS/handlers/serial.py
+++ assignment-03/CUS/handlers/serial.py
@@ -1,8 +1,17 @@
+import asyncio
 import logging
 
 from connections.serial import SerialManager
 
 logger = logging.getLogger(__name__)
+
+CUS_TO_WCS_STATUS_AUTOMATIC = "cus->wcs-st-automatic"
+CUS_TO_WCS_STATUS_REMOTE_MANUAL = "cus->wcs-st-remote-manual"
+CUS_TO_WCS_STATUS_UNCONNECTED = "cus->wcs-st-unconnected"
+CUS_TO_WCS_OPENING_PERCENTEGE_PREFIX = "cus->wcs-op-"
+WCS_TO_CUS_STATUS_AUTOMATIC = "wcs->cus-st-automatic"
+WCS_TO_CUS_STATUS_LOCAL_MANUAL = "wcs->cus-st-local-manual"
+WCS_TO_CUS_OPENING_PERCENTEGE_PREFIX = "wcs->cus-op-"
 
 
 class SerialHandler:
@@ -10,3 +19,9 @@ class SerialHandler:
 
     def __init__(self, connection_manager: SerialManager):
         self._connection_manager = connection_manager
+
+    def handle_message(self, msg):
+        asyncio.create_task(self._process_message_async())
+
+    async def _process_message_async(self):
+        pass  # actual handeling
