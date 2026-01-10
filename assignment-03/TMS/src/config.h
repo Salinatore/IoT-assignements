cc3b7c50dcf5d4edec36826c9de30748cc6b1422
Lorenzo Rossi <lorenzo.rossi50@studio.unibo.it>
2026-01-10 23:43:10 +0100

    add mqtt server name and topic in the config

--- assignment-03/TMS/src/config.h
+++ assignment-03/TMS/src/config.h
@@ -8,5 +8,10 @@
 #define TRIG_PIN 7
 #define POT_PIN A0
 #define SONAR_TIME 38000
+#define WIFI_NAME "tochage"
+#define WIFI_PW "password"
+#define MQTT_SERVER_NAME "broker.mqtt-dashboard.com"
+#define MQTT_TOPIC "esiot-2025-fgor"
+#define MQTT_PREFIX_MSG "tsm->cus-wl-"
 
 #endif
